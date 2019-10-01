f0b53840173ad0171194f248dcfad52faab40865
hclee <hclee@muhayu.com>
2019-10-01 12:52:51 +0900

    chapter 2.4

--- src/test/java/com/hyecheon/ProfileTest.java
+++ src/test/java/com/hyecheon/ProfileTest.java
@@ -18,4 +18,18 @@ public class ProfileTest {
         final boolean matches = profile.matches(criteria);
         assertFalse(matches);
     }
+
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+        final Profile profile = new Profile("Bull Hockey, Inc.");
+        final Question question = new BooleanQuestion(1, "Got bonuses?");
+        final Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+        final Criteria criteria = new Criteria();
+        final Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        final Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+        criteria.add(criterion);
+        final boolean matches = profile.matches(criteria);
+        assertFalse(matches);
+    }
 }
