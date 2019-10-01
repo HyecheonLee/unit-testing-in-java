a23e0a74a55b7be368c93d00b03f2a63e0a061f2
hclee <hclee@muhayu.com>
2019-10-01 12:20:38 +0900

    chapter 1.3

--- src/test/java/com/hyecheon/ScoreCollectionTest.java
+++ src/test/java/com/hyecheon/ScoreCollectionTest.java
@@ -1,12 +1,23 @@
 package com.hyecheon;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 public class ScoreCollectionTest {
     @Test
-    public void test() {
-//        fail("Not yet implemented");
+    public void answersArithmeticMeanOfTwoNumbers() {
+        //준비
+        final ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        //실행
+        final int actualResult = collection.arithmeticMean();
+
+        //단언
+        assertThat(actualResult, equalTo(6));
     }
 }
