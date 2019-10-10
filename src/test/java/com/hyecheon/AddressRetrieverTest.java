105c8378cf23d43691145fa3905e7ea8aadcc658
hclee <hclee@muhayu.com>
2019-10-10 10:43:50 +0900

    chapter 10.5

--- src/test/java/com/hyecheon/AddressRetrieverTest.java
+++ src/test/java/com/hyecheon/AddressRetrieverTest.java
@@ -8,23 +8,22 @@ import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.contains;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AddressRetrieverTest {
     @Test
     public void answersAppropriateAddressForValidCoordinates() throws IOException, ParseException {
-        Http http = (String url) -> {
-            if (!url.contains("lat=38.000000&lon=-104.000000")) {
-                fail("url " + url + " does not contains correct params");
-            }
-            return "{\"address\":{"
-                    + "\"house_number\":\"324\","
-                    + "\"road\":\"North Tejon Street\","
-                    + "\"city\":\"Colorado Springs\","
-                    + "\"state\":\"Colorado\","
-                    + "\"postcode\":\"80903\","
-                    + "\"country_code\":\"us\"}"
-                    + "}";
-        };
+        Http http = mock(Http.class);
+        when(http.get(contains("lat=38.000000&lon=-104.000000"))).thenReturn("{\"address\":{"
+                + "\"house_number\":\"324\","
+                + "\"road\":\"North Tejon Street\","
+                + "\"city\":\"Colorado Springs\","
+                + "\"state\":\"Colorado\","
+                + "\"postcode\":\"80903\","
+                + "\"country_code\":\"us\"}"
+                + "}");
         AddressRetriever retriever = new AddressRetriever(http);
 
         Address address = retriever.retrieve(38.0, -104.0);
